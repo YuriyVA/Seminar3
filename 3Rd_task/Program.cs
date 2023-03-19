0edafbde4110ad4939d34d65acf8e715e56206b6
YuriyVA <yuriy.ramazanov@gmail.com>
2023-03-19 23:27:35 +0300

    3rd

---
+++ 3Rd_task/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+void Cube(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        System.Console.Write(Math.Pow(i, 3)+ " ");
+    }
+}
+
+Cube(num);
